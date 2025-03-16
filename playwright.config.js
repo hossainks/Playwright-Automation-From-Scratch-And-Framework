a83df74ddf6a795aa0ccff5b8f843f1e45d6140c
hossainks <shakirhossain.qa@gmail.com>
2025-03-16 12:52:11 +0600

    Change some data in configuration file

--- playwright.config.js
+++ playwright.config.js
@@ -6,12 +6,13 @@ const { defineConfig, devices } = require('@playwright/test');
  */
 module.exports = defineConfig({
   testDir: './tests',
-  timeout: 40000,
+  timeout: 30 * 1000,
   expect: {
-    timeout: 40000,
+    timeout: 5000,
   },
   reporter: 'html',
   use: {
     browserName: 'chromium',
+    headless: false,
   },
 });
