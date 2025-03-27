df5a0645be7c48e364dba3556b5607b2a0660605
hossainks <shakirhossain.qa@gmail.com>
2025-03-27 20:46:53 +0600

    Select locators using getyRole,getyText with filters

--- tests/angularpractice.spec.js
+++ tests/angularpractice.spec.js
@@ -6,5 +6,17 @@ test('Playwright Special locators', async ({ page }) => {
   // await page.getByLabel('Employed').click();
   await page.getByLabel('Employed').check();
   await page.getByLabel('Gender').selectOption('Female');
+
+  await page.getByPlaceholder('Password').fill('Hello');
+  await page.getByRole('button', { name: 'Submit' }).click();
+  await page
+    .getByText('Success! The Form has been submitted successfully!.')
+    .isVisible();
+  await page.getByRole('link', { name: 'Shop' }).click();
+  await page
+    .locator('app-card')
+    .filter({ hasText: 'Samsung Note 8' })
+    .getByRole('button')
+    .click();
   await page.waitForTimeout(5000);
 });
