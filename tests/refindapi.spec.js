b411f8a1c1579a42691ab129f9dba74eb733c740
hossainks <shakirhossain.qa@gmail.com>
2025-04-03 16:52:02 +0600

    Refactor some other codes

--- tests/refindapi.spec.js
+++ tests/refindapi.spec.js
@@ -1,40 +1,35 @@
 const { test, expect, request } = require('@playwright/test');
-const ApiUtils = require('../utils/apiutils.js');
+const ApiUtils = require('../utils/ApiUtils.js');
 
 let apiUtils;
 let apiContext;
-let token;
 const loginPayload = {
   userEmail: 'manjuk.hossainown@gmail.com',
   userPassword: 'KhaTest123456%',
 };
 
+let productDetails = {
+  _id: '67d70135c019fb1ad62940b1',
+  product: {
+    _id: '67a8df1ac0d3e6622a297ccb',
+    productName: 'ADIDAS ORIGINAL',
+    productCategory: 'fashion',
+    productSubCategory: 'shirts',
+    productPrice: 31500,
+    productDescription: 'Addias Originals',
+    productImage:
+      'https://rahulshettyacademy.com/api/ecom/uploads/productImage_1650649488046.jpg',
+    productRating: '0',
+    productTotalOrders: '0',
+    productStatus: true,
+    productFor: 'women',
+    productAddedBy: 'admin@gmail.com',
+  },
+};
+
 test.beforeAll(async () => {
   apiContext = await request.newContext();
   apiUtils = new ApiUtils(apiContext, loginPayload);
-  token = await apiUtils.getToken(expect);
-
-  // Adding a Product to Cart
-  const productDetails = {
-    _id: '67d70135c019fb1ad62940b1',
-    product: {
-      _id: '67a8df1ac0d3e6622a297ccb',
-      productName: 'ADIDAS ORIGINAL',
-      productCategory: 'fashion',
-      productSubCategory: 'shirts',
-      productPrice: 31500,
-      productDescription: 'Addias Originals',
-      productImage:
-        'https://rahulshettyacademy.com/api/ecom/uploads/productImage_1650649488046.jpg',
-      productRating: '0',
-      productTotalOrders: '0',
-      productStatus: true,
-      productFor: 'women',
-      productAddedBy: 'admin@gmail.com',
-    },
-  };
-  const addToCart = await apiUtils.addProductToCart(productDetails, expect);
-  expect(addToCart.message).toBe('Product Added To Cart');
 });
 
 test.beforeEach(async ({ page }) => {});
@@ -43,10 +38,12 @@ test('Add a product to cart', async ({ page }) => {
   const email = 'manjuk.hossainown@gmail.com';
   page.addInitScript((value) => {
     window.localStorage.setItem('token', value);
-  }, token);
+  }, await apiUtils.getToken(expect));
   await page.goto('https://rahulshettyacademy.com/client');
   await page.waitForLoadState('domcontentloaded');
 
+  const addToCart = await apiUtils.addProductToCart(productDetails, expect);
+  expect(addToCart.message).toBe('Product Added To Cart');
   // Email Details
   const emailText = page.locator("label[type='text']");
 
