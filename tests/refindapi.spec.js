de9f293892e8bbe5f2b04da11f0544f96396ddfd
hossainks <shakirhossain.qa@gmail.com>
2025-04-04 17:23:28 +0600

    Refactor code

--- tests/refindapi.spec.js
+++ tests/refindapi.spec.js
@@ -3,12 +3,13 @@ const ApiUtils = require('../utils/apiutils');
 
 let apiUtils;
 let apiContext;
+
 const loginPayload = {
   userEmail: 'manjuk.hossainown@gmail.com',
   userPassword: 'KhaTest123456%',
 };
 
-let productDetails = {
+const productDetails = {
   _id: '67d70135c019fb1ad62940b1',
   product: {
     _id: '67a8df1ac0d3e6622a297ccb',
@@ -27,15 +28,40 @@ let productDetails = {
   },
 };
 
+const orderPayload = {
+  orders: [
+    {
+      country: 'India',
+      productOrderedId: '67a8df1ac0d3e6622a297ccb',
+    },
+  ],
+};
+
 test.beforeAll(async () => {
   apiContext = await request.newContext();
   apiUtils = new ApiUtils(apiContext, loginPayload);
 });
 
-test.beforeEach(async ({ page }) => {});
-
 test('Add a product to cart', async ({ page }) => {
+  //Locators
   const email = 'manjuk.hossainown@gmail.com';
+  const emailText = page.locator("label[type='text']");
+
+  const productName = 'ADIDAS ORIGINAL';
+  const cart = page.locator("[routerlink*='cart']");
+  const productNameonCart = page.locator("[class='cartSection'] h3");
+  const checkout = page.locator("li[class='totalRow'] button");
+
+  // Orders page
+  const ordersPage = page.locator('h1.ng-star-inserted');
+  const allOrders = page.locator('tbody');
+  const orderSummary = page.locator('.email-title');
+  const orderNumberinSummary = page.locator('.-main');
+
+  // Place order
+  const ordersTab = page.locator("button[routerlink*='myorders']");
+
+  // Actual Test
   page.addInitScript((value) => {
     window.localStorage.setItem('token', value);
   }, await apiUtils.getToken(expect));
@@ -44,15 +70,6 @@ test('Add a product to cart', async ({ page }) => {
 
   const addToCart = await apiUtils.addProductToCart(productDetails, expect);
   expect(addToCart.message).toBe('Product Added To Cart');
-  // Email Details
-  const emailText = page.locator("label[type='text']");
-
-  //const products = page.locator('.card-body');
-  // const cardTitles = page.locator('.card-body b');
-  const productName = 'ADIDAS ORIGINAL';
-  const cart = page.locator("[routerlink*='cart']");
-  const productNameonCart = page.locator("[class='cartSection'] h3");
-  const checkout = page.locator("li[class='totalRow'] button");
 
   await cart.click();
   expect(await productNameonCart.last().textContent()).toBe(productName);
@@ -66,27 +83,9 @@ test('Add a product to cart', async ({ page }) => {
   // Verify email details
   expect(await emailText.textContent()).toBe(email);
 
-  const orderPayload = {
-    orders: [
-      {
-        country: 'India',
-        productOrderedId: '67a8df1ac0d3e6622a297ccb',
-      },
-    ],
-  };
-
   const makeOrder = await apiUtils.createOrder(orderPayload, expect);
   const exactOrderNumer = makeOrder.orders[0];
   console.log(makeOrder.orders[0]);
-
-  // Place order
-  const ordersTab = page.locator("button[routerlink*='myorders']");
-
-  // Orders page
-  const ordersPage = page.locator('h1.ng-star-inserted');
-  const allOrders = page.locator('tbody');
-  const orderSummary = page.locator('.email-title');
-  const orderNumberinSummary = page.locator('.-main');
 
   await ordersTab.click();
   await ordersPage.waitFor();
